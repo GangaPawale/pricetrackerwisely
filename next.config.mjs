cd6d39daaed516c6513b56a843db3637bb715601
Gangaprasad Pawale <10**pawalegp2003@gmail.com>
2024-09-30 16:00:37 +0530

    sooryy for late commit , we have setup databases ,created homepage ,productcard ,serveractions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+     
+      serverComponentsExternalPackages: ['mongoose']
+    },
+    images: {
+      domains: ['m.media-amazon.com']
+    }
+  }
+  
+  export default nextConfig;
+  
