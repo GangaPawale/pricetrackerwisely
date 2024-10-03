a27e29e18cc553a9a36eb06d8d1748d98a15ea57
Gangaprasad Pawale <10**pawalegp2003@gmail.com>
2024-10-03 22:53:31 +0530

    email sender done

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,82 +1,117 @@
 'use client'
 
-import React, { Fragment, useState } from 'react'
+import React, { FormEvent, Fragment, useState } from 'react'
 import { Description, Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
 import Image from 'next/image'
+import { addUserEmailToProduct } from '@/lib/actions'
 
-const Modal = () => {
+interface Props {
+    productId:string
+}
 
-    let [isOpen, setIsOpen] = useState(false)
-    const openModel = () => setIsOpen(true);
-    const closeModel = () => setIsOpen(false);
+const Modal = ({productId}:Props) => {
+    const [isOpen, setIsOpen] = useState(true)
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [email, setEmail] = useState("")
+    
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+    
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('you are in the submit button ')
+        setIsSubmitting(true);
+        await addUserEmailToProduct(productId,email)
+        console.log("you have just passed the setIsSubmitting true")
+        setIsSubmitting(false);
+        console.log('you have just passed the setIsSubmitting(false)')
+        setEmail('');
+        closeModal();
+        console.log("you have closed the model ")
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    }
+
     return (
         <>
-            <button type='button' className='btn' onClick={openModel}>
+            <button type='button' className='btn' onClick={openModal}>
                 Track
             </button>
             <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as='div' onClose={closeModel} className='dialog-container'>
-                    <div className='min-h-screen px-4 text-center  '>
-                        <TransitionChild as={Fragment} enter='ease-out duration-300 ' enterFrom='opacity-0' enterTo='opacity-100'
-                        leave='ease-in duration-300' leaveFrom='opacity-100' leaveTo='opacity-0'
-                        >
-                            <DialogPanel>
+                <Dialog as='div' onClose={closeModal} className='dialog-container'>
+                    <div className='min-h-screen px-4 text-center'>
+                        {/* ... (TransitionChild components remain unchanged) ... */}
+                        
 
-                            </DialogPanel>
-                        </TransitionChild>
-
-                        <span className='inline-block h-screen align-middle'>
-
-                        </span>
-
-                        <TransitionChild as={Fragment} enter='ease-out duration-300 ' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100'
-                        leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'
+                        <TransitionChild
+                            as={Fragment}
+                            enter='ease-out duration-300'
+                            enterFrom='opacity-0 scale-95'
+                            enterTo='opacity-100 scale-100'
+                            leave='ease-in duration-200'
+                            leaveFrom='opacity-100 scale-100'
+                            leaveTo='opacity-0 scale-95'
                         >
                             <div className='dialog-content'>
-                              <div className='flex flex-col'>
-                                    <div className='flex justify-between '>
-                                        <div className='p-3 border border-gray-200 rounded-10'>
-                                            <Image 
-                                            src='/assets/icons/logo.svg'
-                                            alt='logo'
-                                            width={28}
-                                            height={28}
-                                            />
-                                        </div>
+                                {/* ... (dialog content remains unchanged) ... */}
+                                
+                <div className="flex flex-col">
+                  <div className="flex justify-between">
+                    <div className="p-3 border border-gray-200 rounded-10">
+                      <Image 
+                        src="/assets/icons/logo.svg"
+                        alt="logo"
+                        width={28}
+                        height={28}
+                      />
+                    </div>
 
-                                        <Image 
-                                        src='/assets/icons/x-close.svg'
-                                        alt='close'
-                                        width={24}
-                                        height={24}
-                                        className='cursor-pointer'
-                                        onClick={closeModel}
+                    <Image 
+                      src="/assets/icons/x-close.svg"
+                      alt="close"
+                      width={24}
+                      height={24}
+                      className="cursor-pointer"
+                      onClick={closeModal}
+                    />
+                  </div>
+
+                  <h4 className="dialog-head_text">
+                    Stay updated with product pricing alerts right in your inbox!
+                  </h4>
+
+                  <p className="text-sm text-gray-600 mt-2">
+                    Never miss a bargain again with our timely alerts!
+                  </p>
+                </div>
+
+                                <form className='flex flex-col mt-5' onSubmit={handleSubmit} onKeyDown={handleKeyDown}>
+                                    <label htmlFor="email" className='text-sm font-medium text-gray-700'>Email Address</label>
+                                    <div className='dialog-input_container'>
+                                        <Image
+                                            src='/assets/icons/mail.svg'
+                                            alt='mail'
+                                            width={18}
+                                            height={18}
                                         />
-
+                                        <input
+                                            type="email"
+                                            required
+                                            id='email'
+                                            placeholder='Enter your email address'
+                                            className='dialog-input'
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
                                     </div>
-                                    <h4 className='dialog-head_text'>
-                                        Stay updated with your product pricing alerts right in your inbox!
-                                    </h4>
-                                    <p className='text-sm text-gray-600 mt-2 '>
-                                        Never miss the bargain again with the timely alerts
-                                    </p>
-
-                              </div>
-
-                              <form action="" className='flex flex-col mt-5'>
-                                <label htmlFor="email" className='text-sm font-medium text-gray-700'>Email Address</label>
-                                <div className='dialog-input_container'>
-                                    <Image
-                                    src='/assets/icons/mail.svg'
-                                    alt='mail'
-                                    width={18}
-                                    height={18}
-                                    />
-                                    <input type="email" required id='email' placeholder='Enter your email address'  className='dialog-input'/>
-                                </div>
-
-                                <button type='submit' className='dialog-btn'>Track</button>
-                              </form>
+                                    <button type='submit' className='dialog-btn'>
+                                        {isSubmitting ? 'Submitting...' : 'Track'}
+                                    </button>
+                                </form>
                             </div>
                         </TransitionChild>
                     </div>
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,8 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User ,EmailContent} from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 
 export async function scrapeAndStoreProduct(productUrl:string){
@@ -103,6 +105,36 @@ export async function getSimilarProducts(productId:string) {
 
         return similarProducts;
 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export async function addUserEmailToProduct(productId:string,userEmail:string){
+    try {
+
+        const product=await Product.findById(productId);
+
+        if(!product)return ;
+
+        const userExists=await product.users.some((user:User)=>user.email===userEmail)
+
+        if(!userExists) {
+
+            product.users.push({email:userEmail});
+
+            await product.save();
+            console.log("just below product.save in addUserEmailToProduct")
+            const emailContent=await generateEmailBody(product,"WELCOME")
+            console.log("email content" ,emailContent, "email content end")
+            await sendEmail(emailContent,[userEmail]);
+
+            console.log('just below sendEmail')
+            
+        }
+
+        
     } catch (error) {
         console.log(error);
     }
